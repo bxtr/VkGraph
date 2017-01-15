d7c9842b7683a1778426c99df9491e2405dfccf4
bxtr <bxtr@outlook.com>
2017-01-15 16:11:37 +0300

    new tests

--- src/bxtr/GraphFactory.java
+++ src/bxtr/GraphFactory.java
@@ -10,4 +10,5 @@ public class GraphFactory {
         return new UserNode();
     }
 
+
 }
--- src/test/SimpleTest.java
+++ src/test/SimpleTest.java
@@ -33,4 +33,6 @@ public class SimpleTest {
         Assert.assertEquals(false, userTwo.isFriends(userOne));
     }
 
+
+
 }
